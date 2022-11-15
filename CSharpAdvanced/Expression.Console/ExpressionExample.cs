78ba4d886fbbd61fd7c0643ce042d9c6968204e9
Jeff-Chi <asnotracking@gmail.com>
2022-11-15 18:03:59 +0800

    add expression.

---
+++ CSharpAdvanced/Expression.Console/ExpressionExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression.Console
+{
+    public class ExpressionExample
+    {
+        public static void ExpressionTest()
+        {
+
+        }
+    }
+}
